4e06b91b8c4278d41c2a31503f509e4801de4239
Martin Olasz <martin1olasz@gmail.com>
2026-01-17 14:30:53 +0000

    feat: hide tutorials for now

--- src/components/StartMenu/MenuItems.tsx
+++ src/components/StartMenu/MenuItems.tsx
@@ -19,7 +19,7 @@ function renderMenuItem(item: MenuItem) {
 function MenuItems() {
     const menuItems: MenuItem[] = [
         { label: "Start", path: "/play" },
-        { label: "Tutorial", path: "/tutorials" },
+        // { label: "Tutorial", path: "/tutorials" },
         { label: "How to play", path: "/how-to" },
         { label: "Card Library", path: "/library" }
     ]
